ed9651737b50ca470b39e8d32264e4a0c34bb224
Yuvanshankar2005 <144763362+Yuvanshankar2005@users.noreply.github.com>
2026-03-02 10:34:45 +0530

    first poject upload

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class helloworld {
+    public static void main(String[] args) {
+         Scanner sc=new scanner(system.in);
+         System.out.println(x:"enter something");
+         string str= sc.nextline();
+         System.out.println("you entered:"+str);
+         sc.close();
+    }
+
+    
+}
